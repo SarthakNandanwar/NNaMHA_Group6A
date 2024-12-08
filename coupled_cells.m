35e6c33918e24d3c749b6657c6338246d1daf74b
SarthakNandanwar <71076182+SarthakNandanwar@users.noreply.github.com>
2024-12-08 22:54:36 +0100

    Add files via upload
    file for coupled cells

--- coupled_cells.m
+++ coupled_cells.m
@@ -2,7 +2,7 @@ clc;
 clear;
 close all;
 
-
+syms x;
 
 %define parameters under a struct (so it can be accessed in any functions by passing the struct as an input)
 params = struct();
@@ -16,34 +16,35 @@ params.V_b  = 1.1;
 params.R_b  = 1e3;
 params.C    = 10e-9;
 params.R_s  = 200;
+%params.t_delay = 0.1e-4;
 
-params.res_mat =   [1, 2, 3, 2, 5;
-                    2, 1, 3, 4, 5;
-                    3, 3, 1, 3, 5;
-                    2, 4, 3, 1, 5;
-                    5, 5, 5, 5, 1] .* 100;
+%these are the values for the resistor between two cells
+params.res_mat =   [1, 2, 2.5;      %resistance values for connections to the first cell
+                    2, 1, 1.5;      %resistance values for connections to the second cell
+                    2.5, 1.5, 1] .* 1000; %resistance values for connections to the third cell and so on
 
 
 %Initial conditions
 var_mat    =    [0.2, 300;      % V_c1, T_1
-                 0.3, 300;      % V_c2, T_2
-                 0.1, 300;      % V_c3, T_3
-                 0.4, 300;      % V_c4, T_4
-                 0.5, 300]';    % V_c5, T_5
+                 0.4, 300;      % V_c2, T_2
+                 0.3, 300]';    % V_c3, T_3
 
 var_flat = var_mat(:); %flatting the intial condition matrix for input in ODE
+
+%initial time delays for inputs to each circuit
+t_delay = [0, 0.4e-4, 0.6e-4];
 
 t_span = [0:0.0001e-4:2.5e-4];
 
 
-odeFunc = @(t, y) odeMatrix(t, y, params);
+odeFunc = @(t, y) odeMatrix(t, y, params, t_delay);
 [t, y] = ode45(odeFunc, t_span, var_flat);
 
 Y = reshape(y', 2, [], length(t)); %reshape output to a 2*N matrix
 
 
 %plotting separate graphs for each circuit
-figure;
+figure('Position', [100, 100, 800, 600]);
 title('Solutions of the Differential Equations');
 
 for i = 1:size(var_mat,2)
@@ -53,13 +54,13 @@ for i = 1:size(var_mat,2)
     xlabel('Time');
     ylabel('V_'+string(i));
     title('Values of V_'+string(i)');
-    ylim([0.8 1]);
+    %ylim([0.8 1]);
     grid on;
     %hold on;
 end
 
 %plotting overlapping graphs for each circuit
-figure;
+figure('Position', [50, 100, 1500, 300]);
 title('Solutions of the Differential Equations');
 
 for i = 1:size(var_mat,2)
@@ -75,25 +76,32 @@ for i = 1:size(var_mat,2)
 end
 
 %ode function
-function dydt = odeMatrix(t, y, params)
+function dydt = odeMatrix(t, y, params, t_delay)
 
     %define a new matrix that converts y into a matrix
     Y_mat = reshape(y, 2, []);
     output = zeros(size(Y_mat));
+    %V_b = zeros(size(time_delay));
 
 
     sub = calc_sub(Y_mat, params);
     G = @(T, params) params.g_0 * exp(-params.g_1 / T);
     V_mc = @(V_c, T, params) V_c / (1 + G(T, params) * params.R_s);
+    %V_b = @(t) 1.1 * heaviside(t - 0.1e-4);
 
 
     %connect pairs
     for i = 1:size(Y_mat,2)
         V_c = Y_mat(1,i);
         T = Y_mat(2,i);
+        V_b = @(t) (t >= t_delay(i)) * 1.1;
+        %disp(V_b);
 
-        output(:, i) = [  ( ((params.V_b - V_c)/params.R_b)  - (G(T, params) * V_mc(V_c, T, params)) - sub(1,i) ) / params.C;
-                          ( ((V_mc(V_c, T, params) ^ 2) * G(T, params))  - ((T - params.T_0) * params.g_th )) / params.C_th];
+%         output(:, i) = [  ( ((params.V_b - V_c)/params.R_b)  - (G(T, params) * V_mc(V_c, T, params)) - sub(1,i) ) / params.C;
+%                           ( ((V_mc(V_c, T, params) ^ 2) * G(T, params))  - ((T - params.T_0) * params.g_th )) / params.C_th];
+
+          output(:, i) = [  ( ((V_b(t) - V_c)/params.R_b)  - (G(T, params) * V_mc(V_c, T, params)) - sub(1,i) ) / params.C;
+                           ( ((V_mc(V_c, T, params) ^ 2) * G(T, params))  - ((T - params.T_0) * params.g_th )) / params.C_th];
     end
     
     dydt = output(:);
@@ -108,5 +116,6 @@ function out_mat = calc_sub(var_mat, params)
             out_mat(1, i) = out_mat(1, i) + ( (var_mat(1, i) - var_mat(1, j)) / params.res_mat(i,j) ) ;
         end
     end
+    
 
 end
